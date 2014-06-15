030c4c5f75ec26826848a5c06e2249c2673959ca
Kashyap Maduri <kashyap.mad@gmail.com>
2014-06-15 11:29:17 -0700

    extracted super secret stuff

--- kmadswebsite/views.py
+++ kmadswebsite/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("hello world")
+
+def my_view(request):
+    return render(request, 'about_me.html')
